59906b179df76aeee97e3ccc42bfd96350635d77
DYakunin <yakunin_dmitry@irand.ru>
2013-10-08 13:11:11 +0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -54,27 +54,8 @@ describe "AuthenticationPages" do
       
       let(:user) { FactoryGirl.create(:user) }
 
-      describe "in the Users controller" do
-
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_title('Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { put user_path(user) }
-          specify { response.should redirect_to(signin_path) }
-        end
-
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_title('Sign in') }
-        end
-      end
-
       describe "when attempting to visit a protected page" do
-
-	before do
+        before do
           visit edit_user_path(user)
           fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
@@ -82,23 +63,80 @@ describe "AuthenticationPages" do
         end
 
         describe "after signing in" do
+	  
           it "should render the desired protected page" do
             page.should have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+	    
+            before do
+              click_link "Sign out"
+              click_link "Sign in"
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"              
+            end
+
+#            it "should render the default (profile) page" do
+#              page.should have_title(user.name)
+#            end
           end
         end
       end
       
+      describe "in the Users controller" do
+
+	describe "visiting the edit page" do
+	  before { visit edit_user_path(user) }
+	  it { should have_title('Sign in') }
+	end
+
+	describe "submitting to the update action" do
+	  before { put user_path(user) }
+	  specify { response.should redirect_to(signin_path) }
+	end
+
+	describe "visiting the user index" do
+	  before { visit users_path }
+	  it { should have_title('Sign in') }
+	end
+
+	describe "visiting the following page" do
+	  before { visit following_user_path(user) }
+	  it { should have_title('Sign in') }
+	end
+
+	describe "visiting the followers page" do
+	  before { visit followers_user_path(user) }
+	  it { should have_title('Sign in') }
+	end
+      end
+      
+      describe "in the Relationships controller" do
+	
+	describe "submitting to the create action" do
+	  before { post relationships_path }
+	  specify { response.should redirect_to(signin_path) }
+	end
+
+	describe "submitting to the destroy action" do
+	  before { delete relationship_path(1) }
+	  specify { response.should redirect_to(signin_path) }
+	end
+      end
+      
       describe "in the Microposts controller" do
 
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { response.should redirect_to(signin_path) }
-        end
+	describe "submitting to the create action" do
+	  before { post microposts_path }
+	  specify { response.should redirect_to(signin_path) }
+	end
 
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { response.should redirect_to(signin_path) }
-        end
+	describe "submitting to the destroy action" do
+	  before { delete micropost_path(FactoryGirl.create(:micropost)) }
+	  specify { response.should redirect_to(signin_path) }
+	end
       end
     end
 
@@ -109,13 +147,13 @@ describe "AuthenticationPages" do
       before { sign_in user }
 
       describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_title(full_title('Edit user')) }
+	before { visit edit_user_path(wrong_user) }
+	it { should_not have_title(full_title('Edit user')) }
       end
 
       describe "submitting a PUT request to the Users#update action" do
-        before { put user_path(wrong_user) }
-        specify { response.should redirect_to(root_path) }
+	before { put user_path(wrong_user) }
+	specify { response.should redirect_to(root_path) }
       end
     end
     
@@ -126,8 +164,8 @@ describe "AuthenticationPages" do
       before { sign_in non_admin }
 
       describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { response.should redirect_to(root_path) }
+	before { delete user_path(user) }
+	specify { response.should redirect_to(root_path) }
       end
     end
   end
